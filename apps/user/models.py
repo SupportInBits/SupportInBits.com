112a78eefb3476ba1376f9ba61632612f36046b8
SupportInBits <suppportinbits@gmail.com>
2025-06-19 16:02:15 +0200

    añadida función de convertir imagen a webp

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from user.managers import UsuarioManager
+from PIL import Image
+import os
 
 """
 Modelo usuario que hereda de AbstractUser.
@@ -22,6 +24,28 @@ class Usuario(AbstractUser):
     
     objects = UsuarioManager()  # Asigna el manager personalizado
     
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.avatar:
+            img_path = self.avatar.path
+            img = Image.open(img_path)
+
+            # Construir ruta con .webp
+            webp_path = os.path.splitext(img_path)[0] + ".webp"
+
+            # Convertir y guardar en webp
+            img.save(webp_path, "WEBP", quality=85)
+
+            # Eliminar la imagen original si no es webp
+            if not img_path.endswith('.webp'):
+                os.remove(img_path)
+
+            # Cambiar el campo avatar para que apunte al .webp
+            self.avatar.name = os.path.splitext(self.avatar.name)[0] + ".webp"
+            super().save(update_fields=['avatar'])
+
+
     @property
     def es_administrador(self):
         return self.rol == 'administrador' or self.is_superuser
